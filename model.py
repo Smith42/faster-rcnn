41a524a32964797938bfb8acc900df538b149bee
Mike Smith <mikejamesjsmith@gmail.com>
2021-02-24 15:37:58 +0000

    fileloading done

--- model.py
+++ model.py
@@ -1,11 +1,19 @@
 from torchvision.models.detection import fasterrcnn_resnet50_fpn as rcnn
 import torch
+import imageio
 import numpy as np
+import glob
+
+def imloader(fname):
+    """ Load jpg image from fname """
+    return np.asarray(imageio.imread(fname))
 
 if __name__ == "__main__":
     model = rcnn(pretrained=True)
 
-    xs = np.load("data/ims.npy")
+    fis = np.random.permutation(glob.glob("data/livingroom/*.jpg"))[:5]
+
+    xs = np.array(list(map(imloader, fis)))
 
     ps = model(xs) # this returns a list of dicts containing bboxes: ([x1, y1, x2, y2]) labels: (int64) and scores: (float between 0 and 1?)
 
